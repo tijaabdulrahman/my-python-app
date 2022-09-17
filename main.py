4ac39d1c6c94d3fbe2e0e20f1ede50b2c1012a1e
tijani abdulrahman <109444977+tijaabdulrahman@users.noreply.github.com>
2022-09-16 22:48:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+fullName = "Tijani Abdulrahman"
+pi = 3.14
+number = [1, 2, 3, 4, 5]
+age = 41
+isMale = True
+
+
+print(fullName.replace('T', 'm'))
+print(type(pi))
+print(type(number))
+print(type(age))
+print(type(isMale))
